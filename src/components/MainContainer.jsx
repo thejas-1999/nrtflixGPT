47bd95a00cd267284ee9d4626d1d9b317941c7ce
thejas-1999 <thejask074@gmail.com>
2025-10-31 13:59:18 +0530

    add videobackground

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,11 +6,11 @@ const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   if (movies === null) return;
   const mainMovie = movies[0];
-  const { original_title, overview } = mainMovie;
+  const { original_title, overview, id } = mainMovie;
   return (
     <div>
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,4 +1,39 @@
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/507244/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    const filterVideos = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const trailer = filterVideos.length ? filterVideos[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+    console.log(trailer);
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 export default VideoBackground;
