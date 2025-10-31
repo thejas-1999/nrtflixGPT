3c01acc8bdcb0b763883e6e33858d9a14d9b5085
thejas-1999 <thejask074@gmail.com>
2025-10-31 12:13:14 +0530

    setup the movie store

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browser = () => {
+  const dispatch = useDispatch();
   const nowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
-    console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
