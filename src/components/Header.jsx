aa742a446a76cf4931a07f0630312bb83f921bff
thejas-1999 <thejask074@gmail.com>
2025-10-31 11:17:05 +0530

    Add constants

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { useEffect } from "react";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Header = () => {
   const user = useSelector((store) => store.user);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user;
         dispatch(
@@ -28,6 +29,7 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unSubscribe();
   }, []);
 
   const handleSignOut = () => {
@@ -38,11 +40,7 @@ const Header = () => {
 
   return (
     <div className=" w-screen absolute px-8 py-2 bg-linear-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-      />
+      <img src={LOGO} className="w-44" alt="logo" />
       {user && (
         <div className="flex p-4">
           <img src={user?.photoURL} alt="user-icon" className="h-10 w-10" />
