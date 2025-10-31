dc0b6316aa723ac7d2333dd0ada56a19616725f8
thejas-1999 <thejask074@gmail.com>
2025-10-31 13:03:45 +0530

    complete video title

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,11 +1,15 @@
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browser = () => {
   useNowPlayingMovies();
   return (
     <>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </>
   );
 };
---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,4 @@
+const VideoBackground = () => {
+  return <div>VideoBackground</div>;
+};
+export default VideoBackground;
