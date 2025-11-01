5ffe6eab975c32f25c5aeec7e9265538dd6d15a9
thejas-1999 <thejask074@gmail.com>
2025-11-01 12:33:05 +0530

    add popular,top rated,upcoming movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzRlMzQwZjQzYmU5ZDIyYmY2MTMwMTIwZThlOGUxZSIsIm5iZiI6MTY5NTExMTU0OC4zNjcsInN1YiI6IjY1MDk1OTdjM2NkMTJjMDEyZGU5M2YyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C6IQJVkMggggcY9k5ILztv4LS5g76pneXrBEOTlAe8A",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
